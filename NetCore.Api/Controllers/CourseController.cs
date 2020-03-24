1c1c6d95187338334544da7ff4332303f1cab5be
Momir Gjorgjevski <Momir.Gjorgjevski@endava.com>
2020-03-24 17:30:31 +0100

    Added web api project

---
+++ NetCore.Api/Controllers/CourseController.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Web.Http;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+
+namespace NetCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CourseController : Controller
+    {
+       
+    }
+}
