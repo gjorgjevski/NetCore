887afe6c400047860dffda88060bf7147e16cf08
Momir Gjorgjevski <Momir.Gjorgjevski@endava.com>
2020-03-24 19:07:38 +0100

    mediat r

--- NetCore.Api/Controllers/CourseController.cs
+++ NetCore.Api/Controllers/CourseController.cs
@@ -1,8 +1,12 @@
 ﻿
 
 using Microsoft.AspNetCore.Mvc;
+using NetCore.Application.Interfaces;
+using NetCore.Application.ViewModels;
 using System.Collections.Generic;
 using System.Web.Http;
+using FromBodyAttribute = Microsoft.AspNetCore.Mvc.FromBodyAttribute;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
 using RouteAttribute = System.Web.Http.RouteAttribute;
 
 namespace NetCore.Api.Controllers
@@ -10,6 +14,14 @@ namespace NetCore.Api.Controllers
     [Route("api/[controller]")]
     public class CourseController : Controller
     {
-       
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService cs) {
+            _courseService = cs;
+        }
+         
+        public IActionResult Post([FromBody] CourseViewModel cwm){
+            return Ok(cwm);
+        }
     }
 }
--- NetCore.Api/Startup.cs
+++ NetCore.Api/Startup.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using NetCore.Data.Context;
+using NetCore.Ioc;
+using Swashbuckle.Swagger;
 
 namespace NetCore.Api
 {
@@ -23,7 +23,18 @@ namespace NetCore.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UniversityDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("UniversityDbData")));
+
             services.AddMvc();
+
+            
+           
+            services.AddMediatR(typeof(Startup));
+
+            //Call to register out services
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +46,11 @@ namespace NetCore.Api
             }
 
             app.UseMvc();
+        }
+
+        public static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
